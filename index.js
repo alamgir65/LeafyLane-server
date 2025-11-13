3fc2200a74ef81db531115cf5fb8849f00e5b53e
unknown <alamgirhossaindrubo@gmail.com>
2025-11-13 14:25:25 +0600

    Property update system added by api call

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
+const { ObjectId } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
@@ -14,11 +15,11 @@ const uri = "mongodb+srv://leafylaneDBUser:sy0C9KEAbJYSwI4f@alamgir.ilrz28i.mong
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@alamgir.ilrz28i.mongodb.net/?appName=alamgir`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
@@ -28,7 +29,7 @@ app.get('/', (req, res) => {
 
 
 const run = async () => {
-    try{
+    try {
         await client.connect();
 
         const db = client.db("leafyLaneDB");
@@ -55,56 +56,74 @@ const run = async () => {
         // all properties api's are here
 
         app.get('/properties', async (req, res) => {
-            const cursor = propertiesCollection.find();
+            const email = req.query.email;
+            const query = {};
+            if (email) {
+                query.email = email;
+            }
+            console.log(query);
+            const cursor = propertiesCollection.find(query);
             const results = await cursor.toArray();
             res.send(results);
         })
 
-        app.get('/latest-properties', async(req, res) => {
-            const cursor = propertiesCollection.find().sort({created_at :-1}).limit(6);
+        app.patch('/properties/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const updatedProperty = req.body;
+                const query = { _id: new ObjectId(id) };
+
+                const update = {
+                    $set: {
+                        title: updatedProperty.title,
+                        price: updatedProperty.price,
+                        category: updatedProperty.category,
+                        description: updatedProperty.description,
+                        location: updatedProperty.location,
+                        image: updatedProperty.image
+                    }
+                };
+
+                const result = await propertiesCollection.updateOne(query, update);
+                res.send(result);
+            } catch (error) {
+                res.status(500).send({ error: 'Failed to update property' });
+            }
+        });
+
+        app.get('/latest-properties', async (req, res) => {
+            const cursor = propertiesCollection.find().sort({ created_at: -1 }).limit(6);
             const result = await cursor.toArray();
             res.send(result);
         })
 
-        app.post('/properties', async(req, res) => {
+        app.post('/properties', async (req, res) => {
             const newProperty = req.body;
             const result = await propertiesCollection.insertOne(newProperty);
             res.send(result);
         })
 
-        app.get('/properties/:id', async(req, res) => {
+        app.get('/properties/:id', async (req, res) => {
             console.log('property calling');
             const id = req.params.id;
-            const query = {_id : (id)};
+            const query = { _id: (id) };
             const result = await propertiesCollection.findOne(query);
             res.send(result);
         })
 
-        app.delete('/properties/:id' , async(req, res) => {
+        app.delete('/properties/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: id};
+            const query = { _id: id };
             const result = await propertiesCollection.deleteOne(query);
             res.send(result);
         })
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
-    finally{
+    finally {
 
     }
 }
